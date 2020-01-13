05eba1341e2c850624d7c6ed9414d926630bb375
Chenpeihong <chenph@hst.com>
2020-01-13 11:01:54 +0800

    connect to spring-model

---
+++ src/test/java/com/perfect/spring/springmodel/SpringModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfect.spring.springmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
